a9a3e5a490ced199c50a841f749430ae2658e60f
terrycojones <terry@jon.es>
2017-05-16 23:24:25 +0100

    Added logging to 01-stats/sbatch.sh

--- 01-stats/sbatch.sh
+++ 01-stats/sbatch.sh
@@ -9,6 +9,16 @@ echo "01-stats sbatch.sh running at `date`" >> $log
 echo "  Task is $task" >> $log
 echo "  Dependencies are $SP_DEPENDENCY_ARG" >> $log
 
+if [ "$SP_SIMULATE" = "1" ]
+then
+    echo "  Simulating." >> $log
+elif [ "$SP_SKIP" = "1" ]
+then
+    echo "  Skipping." >> $log
+else
+    echo "  Not simulating or skipping. Requesting exclusive node." >> $log
+fi
+
 jobid=`sbatch -n 1 $SP_DEPENDENCY_ARG submit.sh $task | cut -f4 -d' '`
 echo "TASK: $task $jobid"
 
