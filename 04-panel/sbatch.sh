fe6ed7542248346b13a7e79714da5825429e29bd
terrycojones <terry@jon.es>
2017-05-17 00:54:58 +0100

    Make the panel step request an exclusive machine.

--- 04-panel/sbatch.sh
+++ 04-panel/sbatch.sh
@@ -7,7 +7,17 @@ log=$logDir/sbatch.log
 echo "04-panel sbatch.sh running at `date`" >> $log
 echo "  Dependencies are $SP_DEPENDENCY_ARG" >> $log
 
-jobid=`sbatch -n 1 $SP_DEPENDENCY_ARG submit.sh "$@" | cut -f4 -d' '`
+if [ "$SP_SIMULATE" = "1" -o "$SP_SKIP" = "1" ]
+then
+    exclusive=
+    echo "  Simulating or skipping. Not requesting exclusive node." >> $log
+else
+    # Request an exclusive machine so we have enough memory for the panel to finish.
+    exclusive=--exclusive
+    echo "  Not simulating or skipping. Requesting exclusive node." >> $log
+fi
+
+jobid=`sbatch -n 1 $exclusive $SP_DEPENDENCY_ARG submit.sh "$@" | cut -f4 -d' '`
 echo "TASK: panel $jobid"
 
 echo "  Job id is $jobid" >> $log
